5a66d3197baff2df6dc6311695570f46c0b4c45d
Vinod ravi <114280598+vinodravi84@users.noreply.github.com>
2025-07-14 10:04:21 +0530

    Update ImageUpload.js

--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -52,7 +52,7 @@ const ImageUpload = () => {
     formData.append("file", selectedImage);
 
     try {
-      const res = await axios.post("https://detectai-bt9o.onrender.com/api/predict", formData);
+      const res = await axios.post("https://vinod1122-detectai.hf.space/api/predict", formData);
 
       setTimeout(() => {
         setPrediction(res.data.prediction);
