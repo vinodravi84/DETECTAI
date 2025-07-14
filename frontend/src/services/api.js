7d5374c78a2f113919a484d9011c5061ce166442
Vinod ravi <114280598+vinodravi84@users.noreply.github.com>
2025-07-14 10:16:46 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "https://vinod1122-detectai.hf.space/api/predict";
+const apiUrl = "https://vinod1122-detectai.hf.space/api/predict_clip";
 
 export const predictImage = async (file) => {
   const formData = new FormData();
