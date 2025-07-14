20df1de2e6e7357529728e658f277f33d8e67c59
Vinod ravi <114280598+vinodravi84@users.noreply.github.com>
2025-07-14 10:03:33 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "https://detectai-bt9o.onrender.com/api/predict";
+const apiUrl = "https://vinod1122-detectai.hf.space/api/predict";
 
 export const predictImage = async (file) => {
   const formData = new FormData();
